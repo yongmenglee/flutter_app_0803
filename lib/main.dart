d3f8272ac1b7812211f00b15e346ab1c06fa996d
Darren <darrencheng1021@gmail.com>
2020-08-03 16:17:45 +0800

    Flutter Part 1, remove comments.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Flutter Part 1
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-// Run the Flutter App
-// - The arrow notation (=>) is used for one-line functions or methods.
 void main() => runApp(MyApp());
 
 class RandomWords extends StatefulWidget {
@@ -20,11 +20,6 @@ class _RandomWordsState extends State<RandomWords> {
 
   @override
   Widget build(BuildContext context) {
-//    return Container();
-
-//    final wordPair = WordPair.random();
-//    return Text(wordPair.asPascalCase);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
@@ -38,7 +33,6 @@ class _RandomWordsState extends State<RandomWords> {
       padding: EdgeInsets.all((16.0)),
       itemBuilder: (context, i) {
         if (i.isOdd) return Divider();
-
         final index = i ~/ 2;
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
@@ -61,33 +55,8 @@ class _RandomWordsState extends State<RandomWords> {
 // StatelessWidget: the app itself is made a widget.
 class MyApp extends StatelessWidget {
   @override
-
-  // build() method: main job of a widget, describe how to display the widget in
-  // terms of other, lower level widgets
   Widget build(BuildContext context) {
-//    final wordPair = WordPair.random();
-
-    // Material: visual design language => standard on mobile and the web.
-    // - Flutter offers a rich set of Material widgets.
     return MaterialApp(
-//      title: 'Welcome to Flutter',
-//
-//      // Scaffold Widget: Default app bar, title and a body property that holds
-//      // the widget tree for the home screen
-//      home: Scaffold(
-//        appBar: AppBar(
-//          title: Text('Welcome to Flutter'),
-//        ),
-//
-//        // A Center widget containing a Text child widget
-//        // - aligns its widget subtree to the center of the screen
-//        body: Center(
-////          child: Text('Hello World'),
-////          child: Text(wordPair.asPascalCase),
-//          child: RandomWords(),
-//        ),
-//      ),
-
       title: 'Startup Name Generator',
       home: RandomWords(),
     );
