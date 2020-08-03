deb0d76846986b4281744fe4fdb29826a52686f0
Darren <darrencheng1021@gmail.com>
2020-08-03 18:07:10 +0800

    Added package: dart_playground

---
+++ lib/dart_playground/dart_playground_0_test.dart
@@ -0,0 +1,12 @@
+// Define a function.
+printInteger(int aNumber) {
+  print('The number is $aNumber.'); // Print to console.
+}
+
+// This is where the app starts executing.
+main() {
+  print('Hello World!');
+
+  var number = 100; // Declare and initialize a variable.
+  printInteger(number); // Call a function.
+}
